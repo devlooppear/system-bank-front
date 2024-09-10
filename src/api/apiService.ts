ca844af26572a8cdb8de24cc76bb68a797bdb2a9
devlooppear <iago.profissional.developer@gmail.com>
2024-09-10 20:30:44 -0300

    feat: configuring environment

---
+++ src/api/apiService.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const apiService = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  timeout: 5000,
+});
+
+export default apiService;
