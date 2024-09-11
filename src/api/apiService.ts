e9bfa7c52444244ae9f13bf0ed69beb1ef01d011
devlooppear <iago.profissional.developer@gmail.com>
2024-09-11 15:39:43 -0300

    feat: getting jwt token to the requests

--- src/api/apiService.ts
+++ src/api/apiService.ts
@@ -9,4 +9,17 @@ const apiService = axios.create({
   timeout: 5000,
 });
 
+apiService.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("authToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export default apiService;
