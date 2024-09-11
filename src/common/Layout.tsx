d30316dda66954d5447de2e84bd2d0d00522fe35
devlooppear <iago.profissional.developer@gmail.com>
2024-09-11 08:13:08 -0300

    feat: conditions where i gonna have the sidebar

--- src/common/Layout.tsx
+++ src/common/Layout.tsx
@@ -1,14 +1,24 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
 import Sidebar from "./Sidebar";
 
 const Layout = () => {
+  const location = useLocation();
+  const authToken = localStorage.getItem("authToken");
+
+  const routesWithoutSidebar = ["/login", "/register", "/"];
+
+  const shouldHideSidebar =
+    !authToken || routesWithoutSidebar.includes(location.pathname);
+
   return (
     <>
       <NavBar />
       <div className="flex">
-        {window.innerWidth > window.innerHeight && <Sidebar />}
+        {!shouldHideSidebar && window.innerWidth > window.innerHeight && (
+          <Sidebar />
+        )}
         <main className="flex-1">
           <Outlet />
         </main>
