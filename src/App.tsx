0277eb553654690d8f6309c54fd20714f054d701
devlooppear <iago.profissional.developer@gmail.com>
2024-09-11 21:06:24 -0300

    feat: i used a modal for this

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import HomePage from "./pages/HomePage";
 import TransactionPage from "./pages/transactions/TransactionPage";
-import TransactionDetailPage from "./pages/TransactionDetailPage";
 import CreateTransactionPage from "./pages/transactions/CreateTransaction";
 import NoPage from "./pages/NoPage";
 import Dashboard from "./pages/dashboard/Dashboard";
@@ -23,7 +22,6 @@ function App() {
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="transaction" element={<TransactionPage />} />
             <Route path="transaction/create" element={<CreateTransactionPage />} />
-            <Route path="transaction/:id" element={<TransactionDetailPage />} />
           </Route>
 
           <Route path="*" element={<NoPage />} />
